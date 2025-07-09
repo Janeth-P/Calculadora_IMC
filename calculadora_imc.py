57394995b32a413119da666ed5b3de47a20b9dad
Janeth-P <janethalejandra.perez.07@gmail.com>
2025-07-09 17:23:58 -0600

    Update calculadora_imc.py
    Actualizado 9-jul-25

--- calculadora_imc.py
+++ calculadora_imc.py
@@ -8,7 +8,7 @@ edad = int(input("Ingresa tu edad: "))
 peso = float(input("Ingresa tu peso en kg: "))
 estatura = float(input("Ingresa tu estatura en metros (ej. 1.75): "))
 
-# Mostrar los datos ingresados
+# Mostrar los datos ingresados.
 print("\nDatos ingresados:")
 print(f"Nombre completo: {nombre} {apellido_paterno} {apellido_materno}")
 print(f"Edad: {edad} años")
